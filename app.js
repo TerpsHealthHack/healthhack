7c387912dfd68fc33ad0653759bb7589ea5d14af
Alexa <alexacgreenberg@gmail.com>
2014-04-12 17:16:26 -0400

    front/backend, email

---
+++ app.js
@@ -0,0 +1,23 @@
+var mail = require("nodemailer").mail
+ , express = require('express')
+ , app = express()
+ , http = require('http')
+ , server = http.createServer(app)
+ , io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+app.use(express.static(__dirname + '/public'));
+
+
+io.sockets.on('connection', function (socket) {
+    socket.on('send email', function(email) {
+        mail({
+            from: "TerpsHackIt@umd.edu", 
+            to: email.toEmail, 
+            subject: "HERE'S YOUR SCORE!", 
+            text: "Your depression numba is: " + email.healthScore
+        });
+    });
+    socket.emit('connected');
+});
