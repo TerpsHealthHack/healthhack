6d2140b9f584c066cded16b95dbddf385d5529ff
Alexa <alexacgreenberg@gmail.com>
2014-04-13 02:58:29 -0400

    FOUND THE ERROR

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ var mail = require("nodemailer").mail
  , app = express()
  , http = require('http')
  , server = http.createServer(app)
- , io = require('socket.io').listen(server);
+ , io = require('socket.io').listen(server)
+ , MongoClient = require('mongodb').MongoClient;
 
 server.listen(process.env.PORT || 8080);
 
 app.use(express.static(__dirname + '/public'));
+
+var mongoURL = process.env.MONGOHQ_URL;
+
+MongoClient.connect(mongoURL, function(err, db) {
+  if(!err) {
+    console.log("Connected to MongoDB");
+  } 
+  var patients = db.collection('patients');
+  patients.find({'fname' : 'Sally', 'lname' : 'Jones'}).toArray(function(err, items) {
+  	console.log(items);
+  });
+
+});
+
 
 io.sockets.on('connection', function (socket) {
     socket.on('send email', function(email) {
